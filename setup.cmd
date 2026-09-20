3891e2e79d8c1ff74ec14e3d949b7728fe5167ec
dtadptvl <51063200+dtadptvl@users.noreply.github.com>
2026-09-20 08:32:21 +0700

    installer: remove double-click setup entrypoint

--- setup.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal EnableExtensions
-cd /d "%~dp0"
-powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0install.ps1" %*
-set "RC=%ERRORLEVEL%"
-if not defined ZERO_MEM_NO_PAUSE pause
-exit /b %RC%
